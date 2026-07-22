10f71593747a4347663de33e1415211c177dd4f6
Olamilekan-oluwayomi <ilesanmiolamilekan05@gmail.com>
2026-07-22 19:17:26 +0100

    feat: initialize App component with routing for authentication and registration pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import EmailConfirmationPage from './pages/EmailConfirmationPage'
 
 function App() {
-  return <h1>RentIt</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<h1>RentIt Home (placeholder)</h1>} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/confirm" element={<EmailConfirmationPage />} />
+    </Routes>
+  )
 }
 
 export default App
